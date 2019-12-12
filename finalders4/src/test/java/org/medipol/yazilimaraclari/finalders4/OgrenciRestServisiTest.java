1f5bd90c360d76da97df7580fe08ed1a53e9d8bc
dxcvcvvxvx <vxvx>
2019-12-12 20:55:52 +0300

    selamın aleyküm

---
+++ finalders4/src/test/java/org/medipol/yazilimaraclari/finalders4/OgrenciRestServisiTest.java
@@ -0,0 +1,30 @@
+package org.medipol.yazilimaraclari.finalders4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.medipol.yazilimaraclari.finalders4.OgrenciRestServisi.Ogrenci;
+
+class OgrenciRestServisiTest {
+
+	@Test
+	void testAll() {
+		
+	}
+
+	@Test
+	void testNewEmployee() {
+		OgrenciRestServisi servis1= new OgrenciRestServisi();
+		String adSoyad="adSoyad";
+		
+		Ogrenci sonuc = servis1.newEmployee(adSoyad);
+		
+		assertNotNull(sonuc);
+		assertEquals(adSoyad, sonuc.getAdSoyad());
+	}
+
+	@Test
+	void testNewEmployee2() {
+	}
+
+}
